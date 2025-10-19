3790a054737daa7304a2566862d1e2bbd6e72396
Muhammad Fairuz Bin Herman <mfairuz06@gmail.com>
2025-10-19 23:45:35 +0800

    Add files via upload

---
+++ CharacterDIgit.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	
+	char x ;
+	cout << "Input a character"<<endl;
+	cin >> x;
+	
+	if (x>='A'&&x<='z'){
+		
+		cout << "Character";
+		
+		
+	}
+	else if (x>=0 && x<=9){
+		
+		cout << "Digit";
+	}
+	
+	
+	
+}
